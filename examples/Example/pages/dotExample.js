d37a56ec62f81a7cec87ca5efdfd61272131feb0
Shikhar Uttam <shikharuttam@Shikhars-MacBook-Air.local>
2024-11-30 11:25:41 +0530

    modified dot example

--- examples/Example/pages/dotExample.js
+++ examples/Example/pages/dotExample.js
@@ -16,7 +16,7 @@ export default class Example extends Component {
   constructor(){
     super()
     this.data = [
-      {time: '09:00', title: 'Archery Training', description: 'The Beginner Archery and Beginner Crossbow course does not require you to bring any equipment, since everything you need will be provided for the course. ', circleColor: '#009688',lineColor:'#009688'},
+      {time: '09:00', title: 'Archery', description: 'The Beginner Archery and Beginner Crossbow course does not require you to bring any equipment, since everything you need will be provided for the course. ', circleColor: '#009688',lineColor:'#009688'},
       {time: '10:45', title: 'Play Badminton', description: 'Badminton is a racquet sport played using racquets to hit a shuttlecock across a net.'},
       {time: '12:00', title: 'Lunch', icon: require('../img/lunch.png')},
       {time: '14:00', title: 'Watch Soccer', description: 'Team sport played between two teams of eleven players with a spherical ball. ',lineColor:'#009688'},
